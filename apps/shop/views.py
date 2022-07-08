19f8f0399fdbcfe2be8cb113309963993d23e1ff
pdaniel102 <pdaniel@unicauca.edu.co>
2022-07-08 11:14:22 -0500

    add to cart dynamically added

--- apps/shop/views.py
+++ apps/shop/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from matplotlib.style import context
 from numpy import product
+
+from apps.shoppingcart.models import Order
 from .models import *
 # Create your views here.
 def store(request):
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all() 
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items':0}
+        cartItems = order['get_cart_items']
+
+    context = {'items':items, 'order': order}
+
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'store.html', context)
--- apps/shoppingcart/models.py
+++ apps/shoppingcart/models.py
@@ -3,7 +3,7 @@ from apps.shop.models import Product
 
 # Create your models here.
 class Order(models.Model):
-    costumer = models.ForeignKey('usermanagement.Customer', on_delete=models.SET_NULL, blank=True, null=True)
+    customer = models.ForeignKey('usermanagement.Customer', on_delete=models.SET_NULL, blank=True, null=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False, null=True, blank=False)
     transaction_id = models.CharField(max_length=200, null=True)
