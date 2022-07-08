f0c213b64444a83385f5d7415f0e0feab0136189
pdaniel102 <pdaniel@unicauca.edu.co>
2022-07-07 20:12:04 -0500

    cart/checkout dynamic content added

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from numpy import blackman
 
 # Create your models here.
 class Product(models.Model):
@@ -6,7 +7,15 @@ class Product(models.Model):
     price = models.FloatField()
     description = models.CharField(max_length=200)
     digital = models.BooleanField(default=False, null=True, blank=True)
-    #image
+    image = models.ImageField(null= True, blank = True)
 
     def __str__(self):
         return self.name
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
--- apps/shoppingcart/views.py
+++ apps/shoppingcart/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def cart(request):
-    return render(request, 'cart.html')
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(costumer=customer, complete=False)
+        items = order.orderitem_set.all() 
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items':0}
+    context = {'items':items, 'order': order}
+    return render(request, 'cart.html', context)
 
 def checkout(request):
-    return render(request, 'checkout.html')
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(costumer=customer, complete=False)
+        items = order.orderitem_set.all() 
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items':0}
+    context = {'items':items, 'order': order}
+    return render(request, 'checkout.html', context)
