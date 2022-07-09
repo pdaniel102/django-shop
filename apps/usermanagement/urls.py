2f6ef0e6451091101fc746eacf7f50b3eda17a7d
pdaniel102 <pdaniel@unicauca.edu.co>
2022-07-09 10:49:32 -0500

    user registration implemented, login and register button added

--- apps/usermanagement/urls.py
+++ apps/usermanagement/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login, name="login"),
-    path('signup/', views.signup, name="signup"),
+    path("register/", views.register_request, name="register"),
 ]
--- apps/usermanagement/views.py
+++ apps/usermanagement/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import  render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
+
+from apps.usermanagement.models import Customer
 
 # Create your views here.
 
 def login(request):
     return render(request, 'login.html')
 
-def signup(request):
-    return render(request, 'signup.html')
+
+def register_request(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			customer = Customer.objects.create(user=user, name=user.username, email=user.email)
+			messages.success(request, "Registration successful." )
+			return redirect("/")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render (request=request, template_name="signup.html", context={"register_form":form})
